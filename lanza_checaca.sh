d1451da4b23ecb38f9384cb8c3f17c427675baf8
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-09-26 22:04:33 -0500

    añadido desmadre autocaca

---
+++ lanza_checaca.sh
@@ -0,0 +1,4 @@
+# autoreconf --install
+# XXX: https://stackoverflow.com/questions/4553735/gnu-autotools-debug-release-targets
+./configure --with-check=/Users/ernesto/programas/check CFLAGS="-g3"
+make
